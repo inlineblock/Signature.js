679902893e7d3bee11a1490a7985664291576798
Dennis Wilkins <dennis@deliciousmorsel.com>
2012-06-18 21:51:47 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Signature.js
+============
+
+Signature.js is jQuery plugin that allows developers to embed a signature pad into their websites or mobile applications.
+It supports both mouse and touch events, utilizing Modernizr to determine the proper events.
+
+Requirements
+------------
+Signature.js needs 2 things:
+* jQuery v1.0 or greater
+* Modernizr (with touch detection)
+
+License
+-------
+MIT License(LICENSE)
+
+Contribute
+----------
+If you contribute and submit a pull request, I'll will take the time to look over each and ever request.
---
+++ signature.js
@@ -0,0 +1,75 @@
+(function ($) {
+	$.fn.signature = function(options) {
+		if (typeof options == 'string') {
+			var sig;
+			this.each(function() {
+				sig = $(this).find('canvas')[0].toDataURL('image/' + options);
+			});
+			return sig;
+		} else {
+			
+			options = $.extend({
+				strokeColor: '#000000',
+				strokeWidth: 2,
+				backgroundColor: '#FFF',
+				width: 500,
+				height: 250
+			}, options || {});
+
+			this.each(function() {
+				var $this = $(this);
+				$this.html('<canvas></canvas>');
+				var canvas = $this.find('canvas'),
+					context = canvas[0].getContext('2d'),
+					canDraw = false,
+					cancelDraw = function() { canDraw = false; },
+					startDraw = function(evt) { evt.preventDefault(); canDraw = true; previous = {}; },
+					endDraw = function() { canDraw = false; },
+					moveDraw = function (evt) {
+						if (!canDraw) return;
+						if (cancelTimeout) {
+							window.clearTimeout(cancelTimeout);
+						}
+						evt.preventDefault();
+						var x = Math.floor(evt.offsetX),
+							y = Math.floor(evt.offsetY);
+						if (evt.originalEvent && evt.originalEvent.targetTouches) {
+							var touch = evt.originalEvent.targetTouches.item(0),
+								offset = canvas.offset();
+							x = touch.pageX - offset.left;
+							y = touch.pageY - offset.top;
+						}
+						context.strokeStyle = options.strokeColor;
+						context.lineWidth = options.strokeWidth;
+						context.beginPath();
+						context.moveTo(previous.x || x , previous.y || y);
+						context.lineTo(x , y);
+						context.stroke();
+						context.closePath();
+						previous = {x:x , y:y};
+					},
+					previous,
+					cancelTimeout;
+				canvas.attr({
+					width: options.width,
+					height: options.height
+				});
+				context.fillStyle = options.backgroundColor;
+				context.fillRect(0 , 0 , options.width , options.height); // init bg
+				if (Modernizr.touch) {
+					canvas.on('touchstart' , startDraw)
+						.on('touchend' , endDraw)
+						.on('touchcancel' , endDraw)
+						.on('touchmove' , moveDraw);
+
+				} else {
+					canvas.mouseout(function() { cancelTimeout = window.setTimeout(function() { canDraw = false; } , 500); })
+						.mousedown(startDraw)
+						.mouseup(endDraw)
+						.mousemove(moveDraw);
+				}
+			});
+			return this;
+		}
+	};
+})( jQuery );
